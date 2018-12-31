b41aba74d9df4f288358291b84b751393c313589
Subha Manoharan <subhamanoharanbe@gmail.com>
2018-12-31 18:23:16 +0530

    Subha | Add support for db migrations

---
+++ migrations/20181231121252-create-items.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable('items', {
+     id: { type: 'int', primaryKey: true, autoIncrement: true, notNull: true },
+     name: { type: 'string', notNull: true },
+     mrp: { type: 'real', notNull: true, defaultValue: 0 },
+     sp: { type: 'real', notNull: true, defaultValue: 0 }
+   });
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
--- src/repositories/pg-client.ts
+++ src/repositories/pg-client.ts
@@ -1,3 +1,4 @@
 import { Client }  from 'pg';
 
-export default () => new Client({ host: 'localhost', port: 5432, database: 'retail'});
+export default () => new Client({ host: 'localhost', port: 5432, database: 'retail',
+  user: 'postgres', password: 'postgres'});
