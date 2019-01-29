829692822ff4eb5a5d3b6d959cab1fe979ce9c2d
Subha Manoharan <subhamanoharanbe@gmail.com>
2019-01-29 13:29:07 +0530

    Subha | Basic error handling when creating item

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -2,8 +2,13 @@ import * as ItemsRepo from '../repositories/itemsRepo';
 
 const post = async (req, res) => {
   const {name, barcode, sp} = req.body;
-  const id = await ItemsRepo.insert({name, barcode, sp});
-  res.json({id});
+  try{
+    const id = await ItemsRepo.insert({name, barcode, sp});
+    res.json({id});
+  } catch(error){
+    console.error('Error when creating item', error);
+    res.status(400).json({error});
+  }
 };
 
 const all = async (req, res) => {
