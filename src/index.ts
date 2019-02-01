7994a1cfbc8fa0feb227ad7c0036c5c01ea40d24
Subha Manoharan <subhamanoharanbe@gmail.com>
2019-02-01 22:25:05 +0530

    Subha | Handle CORS

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import ItemsRouter from './routes/itemsRoutes';
 const app = express()
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "http://localhost:3001");
+  next();
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/items', ItemsRouter);
