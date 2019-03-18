5a526b1ad3a052304af8a8e8898fceef924f8b9b
Subha Manoharan <subhamanoharanbe@gmail.com>
2019-03-18 12:46:32 +0530

    Subha | Trying to fix heroku build

--- src/routes/itemsRoutes.ts
+++ src/routes/itemsRoutes.ts
@@ -1,12 +1,12 @@
 import * as express from 'express';
 
-import ItemsController from './../controllers/itemsController';
+// import ItemsController from './../controllers/itemsController';
 
 const itemsRouter = express.Router();
 
-itemsRouter.post('/', ItemsController.create);
-itemsRouter.get('/', ItemsController.all);
-itemsRouter.put('/:itemId', ItemsController.update);
-itemsRouter.delete('/:itemId', ItemsController.remove);
+// itemsRouter.post('/', ItemsController.create);
+// itemsRouter.get('/', ItemsController.all);
+// itemsRouter.put('/:itemId', ItemsController.update);
+// itemsRouter.delete('/:itemId', ItemsController.remove);
 
 export default itemsRouter;
