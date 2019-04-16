0cc13ee0f13e8f986f34bb4172a1c21cb9b4345f
Subha Manoharan <subhamanoharanbe@gmail.com>
2019-04-16 09:29:37 +0530

    Subha | Set maxAge for cookie and use memorystore for session

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as path from 'path';
 import * as sessionMiddleware from 'express-session';
+import * as memorystore from 'memorystore';
 import * as config from 'config';
 
 import ApiRouter from './routes/apiRoutes';
@@ -9,11 +10,16 @@ import errorHandler from './services/errorHandler';
 import cors from './middlewares/cors';
 
 const app = express()
+const MemoryStore = memorystore(sessionMiddleware);
 
 app.use(bodyParser.json());
 app.use(cors);
 app.use(sessionMiddleware({ secret: config.SESSION_SECRET,
-  resave: true, saveUninitialized: false }));
+  resave: true, saveUninitialized: false,
+  store: new MemoryStore({
+    checkPeriod: 86400000 // prune expired entries every 24h
+  }),
+  cookie: {httpOnly: true, maxAge: 86400000} }));
 
 app.use(express.static(path.join(__dirname, '../retail-frontend/dist')));
 app.use(express.static(path.join(__dirname, '../retail-frontend/public')));
