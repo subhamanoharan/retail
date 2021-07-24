ee3832d76f870c2bda6139278b3167251c0cf1e4
Subha <subhamanoharanbe@gmail.com>
2021-07-24 21:18:45 +0530

    Subha | Trying IPFilter with error handling

--- src/services/errorHandler.ts
+++ src/services/errorHandler.ts
@@ -1,3 +1,4 @@
+import * as expressIpFilter from 'express-ipfilter';
 import ApplicationError from '../exceptions/applicationError';
 
 export default (err, req, res, next) => {
@@ -6,6 +7,9 @@ export default (err, req, res, next) => {
   }
   if(err instanceof ApplicationError){
     return res.status(err.status).json({errors: err.buildErrors()})
+  }
+  if (err instanceof expressIpFilter.IpDeniedError) {
+    return res.status(401).json({ message: 'You are not allowed to access' });
   }
   console.log('Internal server error', err.message);
   return res.status(500).json({errors: ['Internal server error!']});
