b6aa0f9a463bbdecb315954428121368eb27dfde
Subha <subhamanoharanbe@gmail.com>
2021-09-30 10:43:38 +0530

    Subha | Trying cutting withhex

--- retail-frontend/src/services/driver.js
+++ retail-frontend/src/services/driver.js
@@ -5,12 +5,18 @@ export const sendText = (device, lines) => {
   const encoder = new EscPosEncoder();
 
   const printerEncoder = encoder.initialize();
+  printerEncoder.size('normal');
   lines.forEach(l => printerEncoder.line(l))
   const result = printerEncoder.newline()
     .newline()
     .newline()
     .newline()
-    .raw([0x1B6D])
+    .raw([0x1D5600])
+    .raw([0x1D, 0x56, 0x00])
+    .raw([0x1D5601])
+    .raw([0x1D, 0x56, 0x01])
+    .cut('partial')
+    .cut('full')
     .encode();
   return device.transferOut(3, result);
 }
