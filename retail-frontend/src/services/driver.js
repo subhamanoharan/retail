5c1cbd53442a1b8c5f72c0158a5b1f737cff8268
Subha <subhamanoharanbe@gmail.com>
2021-10-03 21:00:01 +0530

    Subha | Add size

--- retail-frontend/src/services/driver.js
+++ retail-frontend/src/services/driver.js
@@ -5,7 +5,7 @@ export const sendText = (device, lines) => {
   const encoder = new EscPosEncoder();
 
   const printerEncoder = encoder.initialize();
-  printerEncoder.raw([0x1B, 0x21, 0x12])
+  printerEncoder.raw([0x1B, 0x21, 0x21])
 
   lines.forEach(l => printerEncoder.line(l))
   const result = printerEncoder.newline()
