0f415200ca865160d68f4212f3fc1e80bef61399
Subha <subhamanoharanbe@gmail.com>
2021-10-03 20:37:23 +0530

    Subha | Add size

--- retail-frontend/src/services/driver.js
+++ retail-frontend/src/services/driver.js
@@ -5,16 +5,17 @@ export const sendText = (device, lines) => {
   const encoder = new EscPosEncoder();
 
   const printerEncoder = encoder.initialize();
-  printerEncoder.raw([0x1D, 0x21, 0x33]);
   lines.forEach(l => printerEncoder.line(l))
   const result = printerEncoder.newline()
+  //double height
+    .raw([0x1D, 0x21, 0x21])
+    .line('test-line-test-line-test-line-test-line-test-line-21')
+    //double width
+    .raw([0x1D, 0x21, 0x12])
+    .line('test-line-test-line-test-line-test-line-test-line-12')
     .newline()
     .newline()
     .newline()
-    .raw([0x1D, 0x21, 0x22])
-    .line('test-line-test-line-test-line-test-line-test-line-22')
-    .raw([0x1D, 0x21, 0x33])
-    .line('test-line-test-line-test-line-test-line-test-line-33')
     // command for full cut
     .raw([0x1D, 0x56, 0x00])
     .encode();
