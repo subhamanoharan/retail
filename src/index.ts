adb64ca677eba30374fa5c20b205461c3d9a7169
Your Name <subhamanoharanbe@gmail.com>
2022-12-14 17:30:57 +0530

    Subha | Add config port

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ import app from './app';
 
 app.listen(config.PORT, () => console.log('App listening on port-', config.PORT))
 
-if(process.env.NODE_ENV == 'production') {
-  console.log("Setting up HTTPS")
-  var options = {
-      key: fs.readFileSync('/etc/letsencrypt/live/msg-billing.in/privkey.pem'),
-      cert: fs.readFileSync('/etc/letsencrypt/live/msg-billing.in/fullchain.pem'),
-      ca: fs.readFileSync('/etc/letsencrypt/live/msg-billing.in/chain.pem')
-  };
-  const httpsServer = https.createServer(options, app)
-  httpsServer.listen(config.HTTPS_PORT, () => console.log('App listening on port-443'))
-}
+// if(process.env.NODE_ENV == 'production') {
+//   console.log("Setting up HTTPS")
+//   var options = {
+//       key: fs.readFileSync('/etc/letsencrypt/live/msg-billing.in/privkey.pem'),
+//       cert: fs.readFileSync('/etc/letsencrypt/live/msg-billing.in/fullchain.pem'),
+//       ca: fs.readFileSync('/etc/letsencrypt/live/msg-billing.in/chain.pem')
+//   };
+//   const httpsServer = https.createServer(options, app)
+//   httpsServer.listen(config.HTTPS_PORT, () => console.log('App listening on port-443'))
+// }
